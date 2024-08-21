c9fd4f0cffd32c77b273258ff57488a2625d96b0
Leo Araya <larayavrs@gmail.com>
2024-08-21 16:18:00 -0400

    feat!: add build and run scripts for Zylo project

---
+++ scripts/build.bat
@@ -0,0 +1,6 @@
+@echo off
+REM Ensure the build directory exists
+if not exist build mkdir build
+
+REM Compile the project
+g++ -o ../build/zylo ../src/main.cxx ../src/terminal.cxx -I../src -I../src/utilities -std=c++17
---
+++ scripts/run.bat
@@ -0,0 +1,2 @@
+cd ../build
+.\zylo.exe
