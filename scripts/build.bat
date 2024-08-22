73153281105fcc45665165bfbea79311548f4dcf
Leo Araya <larayavrs@gmail.com>
2024-08-22 14:28:23 -0400

    refactor: update build script to use relative paths for source files

--- scripts/build.bat
+++ scripts/build.bat
@@ -3,4 +3,4 @@ REM Ensure the build directory exists
 if not exist build mkdir build
 
 REM Compile the project
-g++ -o ../build/zylo ../src/main.cxx ../src/terminal.cxx -I../src -I../src/utilities -std=c++17
+g++ -o ./build/zylo ./src/main.cxx ./src/terminal.cxx -I./src -I./src/utilities -std=c++17
